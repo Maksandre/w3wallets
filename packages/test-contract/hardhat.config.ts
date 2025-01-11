49ec27e99be0e97bbf4e208bd9ac097b86adca90
Max Andreev <maxick20@gmail.com>
2025-01-11 11:44:15 +0700

    configure hh project

--- packages/test-contract/hardhat.config.ts
+++ packages/test-contract/hardhat.config.ts
@@ -1,13 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomicfoundation/hardhat-verify";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "./tasks/storage";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-    },
+    hardhat: {},
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
     },
   },
   solidity: "0.8.28",

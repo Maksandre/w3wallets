c9b47441b4d4057a8200e66ac73566a177684084
Max Andreev <maxick20@gmail.com>
2025-02-10 22:37:03 +0500

    backpack: add/switch accounts

--- packages/w3wallets/src/backpack/backpack.ts
+++ packages/w3wallets/src/backpack/backpack.ts
@@ -4,6 +4,8 @@ import { Wallet } from "../wallet";
 
 export class Backpack extends Wallet {
   private defaultPassword = "11111111";
+  private currentAccountId = 0;
+  private maxAccountId = 0;
 
   async gotoOnboardPage() {
     await this.page.goto(
@@ -13,21 +15,24 @@ export class Backpack extends Wallet {
   }
 
   async onboard(network: BackPackNetwork, privateKey: string) {
-    await this.page.getByRole("button", { name: "Import wallet" }).click();
-    await this.page.getByRole("button", { name: network }).click();
-    await this.page.getByRole("button", { name: "Import private key" }).click();
-    await this.page.getByPlaceholder("Enter private key").fill(privateKey);
-    await this.page.getByRole("button", { name: "Import" }).click();
-    await this.page
-      .getByPlaceholder("Password", { exact: true })
-      .fill(this.defaultPassword);
-    await this.page
-      .getByPlaceholder("Confirm Password")
-      .fill(this.defaultPassword);
-    await this.page.getByRole("checkbox").click();
-    await this.page.getByRole("button", { name: "Next" }).click();
-    await expect(this.page.getByText("You're all good!")).toBeVisible();
-    await this.page.goto(`chrome-extension://${this.extensionId}/popup.html`);
+    this.currentAccountId++;
+    this.maxAccountId++;
+    return this._importAccount(network, privateKey, true);
+  }
+
+  async addAccount(network: BackPackNetwork, privateKey: string) {
+    this.maxAccountId++;
+    this.currentAccountId = this.maxAccountId;
+    await this.page.goto(
+      `chrome-extension://${this.extensionId}/options.html?add-user-account=true`,
+    );
+    await this._importAccount(network, privateKey, false);
+  }
+
+  async switchAccount(id: number) {
+    await this.page.getByRole("button", { name: `A${this.currentAccountId}` }).click();
+    await this.page.getByRole("button", { name: `Account ${id}` }).click();
+    this.currentAccountId = id;
   }
 
   async unlock() {
@@ -35,13 +40,10 @@ export class Backpack extends Wallet {
     await this.page.getByRole("button", { name: "Unlock" }).click();
   }
 
-  async goToSettings(accountName?: string) {
-    await this.page.getByRole("button", { name: accountName ?? "A1" }).click();
-    await this.page.getByRole("button", { name: "Settings" }).click();
-  }
-
   async setRPC(network: BackPackNetwork, rpc: string) {
-    await this.goToSettings();
+    await this._clickOnAccount();
+    await this.page.getByRole("button", { name: "Settings" }).click();
+
     await this.page.getByRole("button", { name: network }).click();
     await this.page.getByRole("button", { name: "RPC Connection" }).click();
     await this.page.getByRole("button", { name: "Custom" }).click();
@@ -60,5 +62,34 @@ export class Backpack extends Wallet {
 
   async deny() {
     await this.page.getByText("Deny", { exact: true }).click();
+  }
+
+  private async _clickOnAccount() {
+    return this.page.getByRole("button", { name: `A${this.currentAccountId}`, exact: true }).click();
+  }
+
+  private async _importAccount(
+    network: BackPackNetwork,
+    privateKey: string,
+    isOnboard: boolean,
+  ) {
+    await this.page.getByRole("button", { name: "Import wallet" }).click();
+    await this.page.getByRole("button", { name: network }).click();
+    await this.page.getByRole("button", { name: "Import private key" }).click();
+    await this.page.getByPlaceholder("Enter private key").fill(privateKey);
+    await this.page.getByRole("button", { name: "Import" }).click();
+    if (isOnboard) {
+      await this.page
+        .getByPlaceholder("Password", { exact: true })
+        .fill(this.defaultPassword);
+      await this.page
+        .getByPlaceholder("Confirm Password")
+        .fill(this.defaultPassword);
+
+        await this.page.getByRole("checkbox").click();
+        await this.page.getByRole("button", { name: "Next" }).click();
+    }
+    await expect(this.page.getByText("You're all good!")).toBeVisible();
+    await this.page.goto(`chrome-extension://${this.extensionId}/popup.html`);
   }
 }

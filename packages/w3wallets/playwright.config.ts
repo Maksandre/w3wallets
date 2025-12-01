b45430ce83166c0b6fd9cf947ef028db0c3ae1d8
Max Andreev <maxick20@gmail.com>
2025-12-01 15:30:34 +0500

    add traces

--- packages/w3wallets/playwright.config.ts
+++ packages/w3wallets/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     baseURL: config.baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     headless: true,
     screenshot: "only-on-failure",
   },

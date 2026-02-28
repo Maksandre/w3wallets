37b59a2edb8568005c17e4fd2340e9c6357a7566
Max Andreev <maxick20@gmail.com>
2026-02-28 22:54:55 +0500

    add max failure of 5 tests

--- packages/w3wallets/playwright.config.ts
+++ packages/w3wallets/playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  maxFailures: 5,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
